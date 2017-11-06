14771daf2478c4055357fb0c34a9a687d575b7f9
Diego Burlando <diego@dmm888.com>
2017-11-06 11:19:43 +0100

    Initialized project using create-react command line utilities

---
+++ src/BeyondFeedsManagement.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import './BeyondFeedsManagement.css';
+
+class BeyondFeedsManagement extends Component {
+
+constructor(props) {
+    super(props);
+    this.state = { data: [], };
+}
+
+componentDidMount() {
+
+fetch("https://www.googleapis.com/youtube/v3/playlistItems?part=snippet,contentDetails,status&maxResults=10&playlistId=PLSi28iDfECJPJYFA4wjlF5KUucFvc0qbQ&key=AIzaSyCuv_16onZRx3qHDStC-FUp__A6si-fStw")
+      .then(response => response.json())
+      .then(json => {        
+        this.setState({
+          data: json.items
+        });        
+      });
+}
+
+componentWillMount() {}
+componentWillUnmount() {}
+
+shouldComponentUpdate(nextProps,nextState) {
+  return true; 
+}
+
+
+componentWillReceiveProps(nextProps) {}
+getInitialState() {}
+
+render() {
+
+  let feeds = this.state.data.map(function(item) {
+      return (<p key = {item.kind}><span>{item.snippet.title}</span>&nbsp;
+        <textarea defaultValue={item.snippet.description} rows="10" cols="20"></textarea>     
+        </p>);
+  });
+
+  return (
+    <div className="App">    
+      {feeds}
+    </div>
+  );
+
+}
+}
+
+
+export default BeyondFeedsManagement;
